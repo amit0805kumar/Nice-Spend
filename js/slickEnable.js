80e5fc9cadf5aea1528b0b126577a901712fe275
Amit Kumar <32258879+amit0805kumar@users.noreply.github.com>
2020-10-05 19:40:03 +0530

    single img banner created

---
+++ js/slickEnable.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  $(".slick_single").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+});
